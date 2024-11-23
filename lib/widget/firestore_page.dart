46e4e2f92a8409864fc4dee39d9f036c136164a8
sixtick_sub3 <business4dyd@gmail.com>
2024-11-23 10:02:40 +0900

    .

--- lib/widget/firestore_page.dart
+++ lib/widget/firestore_page.dart
@@ -17,14 +17,11 @@ class User{
     try{
       CollectionReference ref = FirebaseFirestore.instance.collection('user');
       final querySnapshot = await ref.get(); 
-      // debugPrint(ref.id);
       debugPrint(querySnapshot.size.toString());
-      ref.doc('user${querySnapshot.size + 1}').set(toMap());
+      ref.doc('user${querySnapshot.size + 1}').set(toMap());// 없으면 만들고 있으면 업데이트
     }catch (e){
       debugPrint('add() 예외 : ${e.toString()}');
     }
-    
-    // await ref.add(toMap());
   }
 
   Future<void> update() async {
@@ -34,16 +31,29 @@ class User{
     // 헐 겁나 빠르네?? 
   }
 
+  Future<void> test() async {
+    CollectionReference ref = FirebaseFirestore.instance.collection('user');
+    ref.where('age', isGreaterThan: 30)
+    .orderBy('age', descending: true)
+    .limit(1)
+    .get()
+    .then((value){
+      value.docs.forEach((item){
+        debugPrint('result : ${item.get('name')}');
+      });
+    }).catchError((e) => debugPrint('test error : $e'));
+  }
+
   // Future<void> 
 }
 
 class FirestorePage extends StatelessWidget{
-  User user2 = User(age: 43, name: 'kim', email: 'a@a.com');
+  User user2 = User(age: 42, name: 'cho', email: 'a@a.com');
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ElevatedButton(onPressed: (){user2.update();}, child: const Text('테스트')),
+        ElevatedButton(onPressed: (){user2.test();}, child: const Text('테스트')),
         const Text('테스트중'),
       ],
     );
